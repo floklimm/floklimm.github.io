8c8046c740ff9611022e8a1beaf70efbe11f134b
floklimm <f.klimm@gmail.com>
2021-12-09 23:00:00 +0100

    stronger force

--- authorshipforceLayout.js
+++ authorshipforceLayout.js
@@ -25,7 +25,7 @@ var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().distanceMin(25).distanceMax(100).strength(-30))
+    .force("charge", d3.forceManyBody().distanceMin(25).distanceMax(100).strength(-40))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 // Define the div for the tooltip
